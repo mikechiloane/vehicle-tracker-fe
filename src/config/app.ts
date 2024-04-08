306cf86dfe344c92de05a0d5534801b640cf248e
Mike Chiloane <125856129+chiloanemike@users.noreply.github.com>
2024-04-08 07:44:29 +0200

    app commit

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,8 +1,7 @@
-// export const GOOGLE_MAPS_API_KEY: string | undefined =  process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 export const GOOGLE_MAPS_API_KEY: string = "AIzaSyDzqH5VmjOfIFgGrSYOj9Cf8qqMP73JViw";
-export const LOCATION_SERVICE_URL: string = "http://localhost:8081/api/v1/vehicle";
-export const SOCKETIO_URL: string = "http://192.168.0.196:8080";
+export const LOCATION_SERVICE_URL: string = "http://localhost:8080/api/v1/vehicle";
+export const SOCKETIO_URL: string = "http://localhost:8082";
 
 export const API_URLS = {
     location: "/location",
